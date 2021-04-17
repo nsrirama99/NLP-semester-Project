3099e6bb2aecbac60488ff2cbe82efdd7ca56643
Nathan Srirama <nsrirama@smu.edu>
2021-04-17 16:49:22 -0500

    added some pdf reading to openIEtest

--- openIEtest.py
+++ openIEtest.py
@@ -6,11 +6,38 @@ Created on Sat Apr 10 17:39:15 2021
 """
 
 from openie import StanfordOpenIE
+import PyPDF2 as pdf
 
 #with StanfordOpenIE() as client:
 client = StanfordOpenIE()
 
 text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence'
+
+dirName = input("please enter a directory to parse files from \(must be relative path from current directory\)\n")
+path = os.path.join(os.getcwd(), dirName)
+
+for file in [f for f in os.listdir(path) if f.endswith('.txt')]: 
+    with open(os.path.join(path, file), 'r') as r:
+    corpus = r.read().replace('\n', ' ').replace('\r', '')
+        
+    triple_corpus = client.annotate(corpus)
+    print('Corpus: %s [...].' % corpus)
+    print('Found %s triples in the corpus.' % len(triple_corpus))
+    for triple in triple_corpus:
+        print('|-', triple)
+    
+for file in[f for f in os.listdir(path) if f.endswith('.pdf')]:
+    text = ''
+    pdfFile = open(os.path.join(path, file), 'rb')
+    pdfReader = pdf.PdfFileReader(pdfFile)
+    for x in range(pdfReader.numPages):
+        pageObj = pdfReader.getPage(x)
+        text += pageObj.extractText()
+    pdfFile.close()
+    
+    text.replace('\n', ' ').replace('\r', '')
+    
+
 print('Text: %s.' % text)
     
 for triple in client.annotate(text):
@@ -19,12 +46,3 @@ for triple in client.annotate(text):
 #graph_image = 'graph.png'
 #client.generate_graphviz_graph(text, graph_image)
 #print('Graph generated: %s' % graph_image)
-   
-with open('test.txt', 'r') as r:
-    corpus = r.read().replace('\n', ' ').replace('\r', '')
-        
-triple_corpus = client.annotate(corpus)
-print('Corpus: %s [...].' % corpus)
-print('Found %s triples in the corpus.' % len(triple_corpus))
-for triple in triple_corpus:
-    print('|-', triple)
